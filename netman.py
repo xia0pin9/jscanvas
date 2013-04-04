c7880a6f63082cceeb26a0688e411d2574e390f5
xia0pin9 <lyp20062392@gmail.com>
2013-04-04 16:48:21 -0500

    prototype structure

---
+++ netman.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+This script is a prototype DSL for network management.
+"""
+
+import os
+import sys
+import getopt
+import scapy
+
+
+def usage():
+    print 
+
+def interact():
+
+
+class NetMan:
+    def __init__(self, ):
+
+    def getnet():
+
+    def gethost():
+
+    def traceroute():
+
+    def watchservices():
+
+    def sniffer():
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
