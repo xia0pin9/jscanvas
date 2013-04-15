938e2bfad8e55bcfa0902712b98680e930e43820
xia0pin9 <lyp20062392@gmail.com>
2013-04-14 21:47:52 -0500

    Modify php code

--- php/config.php
+++ php/config.php
@@ -7,7 +7,7 @@ $database = "snort";
 //or die("Opps some thing went wrong");
 //mysql_select_db($database, $bd) or die("Opps some thing went wrong");
 $mysqli = new mysqli($hostname, $user, $password, $database);
-if ($mysqli->connect_errno){
-    die("Mysql connect error: ".$mysqli->connect_error);
+if (mysqli_connect_errno()){
+    exit("Mysql connect error: ".mysqli_connect_error());
 }
 ?>
